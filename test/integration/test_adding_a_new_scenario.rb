b92603f415154cad4d973ec38f907e8acf2ce4e3
Brennan Willingham <brennanw22@gmail.com>
2015-05-11 10:12:20 -0500

    Added scenario file.

---
+++ test/integration/test_adding_a_new_scenario.rb
@@ -0,0 +1,17 @@
+# In order to have illuminating connections in out quiz results
+# I want to add many scenarios
+
+# Usage Example:
+
+#   > ./AshTray
+#   1. Add a cigar.
+#   2. Describe the composition of the cigar.
+#   3. List the cigars, thier makeup, and reviews.
+
+# Accaptence Criteria:
+
+#   * Program allows for entries
+#   * Lists of cigars populates
+#   * Option to return to menu
+#   * Deleted cigars are removed from the database
+#   * After Deletion the use is returned to the menu
