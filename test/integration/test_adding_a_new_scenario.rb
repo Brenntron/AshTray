638a7446884529e942c4c4737a3f96831a16f6c9
Brennan Willingham <brennanw22@gmail.com>
2015-05-11 10:52:13 -0500

    Added minimum argument test to integration folder.

--- test/integration/test_adding_a_new_scenario.rb
+++ test/integration/test_adding_a_new_scenario.rb
@@ -1,9 +1,11 @@
+require_relative "../test_helper"
+
 # In order to have illuminating connections in out quiz results
 # I want to add many scenarios
 
 # Usage Example:
 
-#   > ./AshTray
+#   > ./AshTray cigar
 #   1. Add a cigar.
 #   2. Describe the composition of the cigar.
 #   3. List the cigars, thier makeup, and reviews.
@@ -15,3 +17,17 @@
 #   * Option to return to menu
 #   * Deleted cigars are removed from the database
 #   * After Deletion the use is returned to the menu
+
+class AddingANewScenarioTest < Minitest:Test
+
+  def test_minimum_arguments_required
+    shell_output    = ""
+    expected_output = ""
+    IO.popen('./ash_tray') do |pipe|
+      expected_output = "[Help] Run as: ./ash_tray new"
+      shell_output = pipe.read
+    end
+    assert_equal expected_output, shell_output
+  end
+
+end
