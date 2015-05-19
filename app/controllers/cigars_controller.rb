c02e8b6a0b4db43ea62e7bb1e60a043dd8731d7a
Brennan Willingham <brennanw22@gmail.com>
2015-05-19 16:50:30 -0500

    Added the makers part and the valid? that I completly forgot about....

--- app/controllers/cigars_controller.rb
+++ app/controllers/cigars_controller.rb
@@ -6,7 +6,7 @@ class CigarsController
       cigars = Cigar.all
       cigars_string = ""
       cigars.each_with_index do |cigar, index|
-        cigars_string << "#{index + 1}. #{cigar.name} #{cigar.length} {cigar.model}\n"
+        cigars_string << "#{index + 1}. #{cigar.maker} #{cigar.name} #{cigar.length} {cigar.model}\n"
       end
       cigars_string
     else
@@ -14,22 +14,28 @@ class CigarsController
     end
   end
 
-  def add(name, length, ring_gauge)
+  def add(maker, name, length, ring_gauge)
     cigar            = Cigar.new
+    cigar.maker      = maker
     cigar.name       = name
     cigar.length     = length
     cigar.ring_gauge = ring_gauge
     if cigar.save_cigar
-      "#{name} #{length} #{ring_gauge} has been added\n"
+      "#{maker} #{name} #{length} #{ring_gauge} has been added\n"
     else
       "#{cigar.errors}"
     end
   end
 
   def new_cigar
+    maker      = ask("Who made the cigar?")
+    while maker.rstrip.empty?
+      puts "\"#{maker}\" is unacceptable!"
+      maker = ("Who made the cigar")
+    end
     name       = ask("What is the name of the cigar?")
     while name.rstrip.empty?
-      puts "\#{name}\" in unacceptable!"
+      puts "\"#{name}\" is unacceptable!"
       name = aks("What is the name of the cigar?")
     end
     length     = ask("What is the length of the cigar?")
@@ -43,7 +49,7 @@ class CigarsController
       puts "\"#{ring_gauge}\" is unacceptable!"
       ring_gauge = ask("What is the ring gauge?")
     end
-    response = self.add(name, length, ring_gauge)
+    response = self.add(maker, name, length, ring_gauge)
     say(response) unless response.nil?
   end
 
--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -1,10 +1,11 @@
 class Cigar
   attr_reader :id, :errors
-  attr_accessor :name, :length, :ring_gauge
+  attr_accessor :maker, :name, :length, :ring_gauge
 
   def self.all
     Database.execute("select * from cigars").map do |row|
       cigar = Cigar.new
+      cigar.maker      = row['maker']
       cigar.name       = row['name']
       cigar.length     = row['length']
       cigar.ring_gauge = row['ring_gauge']
@@ -18,7 +19,9 @@ class Cigar
   end
 
   def valid?
-    if @name.rstrip.empty?
+    if @maker.rstrip.empty?
+      @errors = "\"#{maker}\" is not a valid name."
+    elsif @name.rstrip.empty?
       @errors = "\"#{name}\" is not a valid name."
       false
     elsif @length.rstrip.empty?
@@ -31,14 +34,33 @@ class Cigar
     end
   end
 
+  def valid?
+    if @maker.rstrip.empty?
+      @errors = "\"#{maker}\" is unacceptable!"
+      false
+    elsif @name.rstrip.empty?
+      @errors = "\"#{name}\" is unacceptable!"
+      false
+    elsif @length.rstrip.empty?
+      @errors = "\"#{length}\" is unacceptable!"
+      false
+    elsif !@ring_gauge.class == Fixnum or @ring_gauge.to_i.zero?
+      @errors = "\"#{ring_gauge}\" is unacceptable!"
+      false
+    else
+      @errors = nil
+      true
+    end
+  end
+
   def save
     return false unless valid?
     if @id.nil?
-      Database.execute("INSERT INTO cigars (name, length, ring_gauge) VALUES (?)", name, length, ring_gauge)
+      Database.execute("INSERT INTO cigars (maker, name, length, ring_gauge) VALUES (?)", maker, name, length, ring_gauge)
       @id = Database.execute("SELECT last_insert_rowid()")[0]['last_insert_rowid()']
       true
     else
-      Database.execute("UPDATE cigars SET name = ?, length = ?, ring_gauge = ? WHERE id = ?", name, length, ring_gauge, id)
+      Database.execute("UPDATE cigars SET maker = ?, name = ?, length = ?, ring_gauge = ? WHERE id = ?", maker, name, length, ring_gauge, id)
     end
   end
 end
